6d76a3af3778519c80910f2befd3b72ed193e5ad
Juan Lladó <jllado@gmail.com>
2017-04-20 01:55:21 +0200

    React minimal setup

---
+++ config/webpack.config.babel.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+                loader: 'url-loader?limit=100000&name=[name].[ext]'
+            },
+            {test: /\.css$/, loader: 'style-loader!css-loader'},
+            {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './public/index.html', favicon: './public/favicon.ico'})
+    ]
+};
